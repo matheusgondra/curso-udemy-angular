d1ac7455add71350e20813859415a43509eed01b
matheusgondra <matheusgondra951@gmail.com>
2025-12-15 09:14:57 -0300

    criando primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HelloWorldComponent } from "./helloworld/helloworld.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	imports: [RouterOutlet, HelloWorldComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-  title = 'conceitos-basicos';
+	title = "conceitos-basicos";
 }
