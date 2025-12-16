92207b87cbea82ef1ca3b62de00aa1189fa3f33d
matheusgondra <matheusgondra951@gmail.com>
2025-12-16 12:48:18 -0300

    adicionando ação no componente

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-calculadora',
-  imports: [],
-  templateUrl: './calculadora.component.html',
-  styleUrl: './calculadora.component.scss'
+	selector: "app-calculadora",
+	imports: [],
+	templateUrl: "./calculadora.component.html",
+	styleUrl: "./calculadora.component.scss"
 })
 export class CalculadoraComponent {
+	numero1 = 0;
+	numero2 = 0;
+	resultado = 0;
 
+	calcularResultado() {
+		this.resultado = this.numero1 + this.numero2;
+	}
 }
