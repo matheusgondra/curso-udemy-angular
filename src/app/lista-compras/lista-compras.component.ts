51b6dd42738e05bc2a6aff7da11854223c165015
matheusgondra <matheusgondra951@gmail.com>
2025-12-30 09:19:00 -0300

    formulario de adição de items

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-lista-compras',
-  imports: [],
-  templateUrl: './lista-compras.component.html',
-  styleUrl: './lista-compras.component.scss'
+	selector: "app-lista-compras",
+	imports: [FormsModule],
+	templateUrl: "./lista-compras.component.html",
+	styleUrl: "./lista-compras.component.scss"
 })
 export class ListaComprasComponent {
+	item = "";
 
+	adicionarItem() {
+		console.log("Item adicionado:", this.item);
+		this.item = "";
+	}
 }
